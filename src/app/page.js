67f4b22f68507ff89094ba3fb8275c0d8a51725a
Kanish Kumar <kanishkumar11@outlook.in>
2024-03-10 17:27:42 +0530

    testing...

--- src/app/page.js
+++ src/app/page.js
@@ -14,26 +14,20 @@ import Loading from "@/components/Loading";
 const Page = () => {
   const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  //   // Simulating a loading delay, replace the setTimeout with your actual data fetching logic
-  //   const loadingTimeout = setTimeout(() => {
-  //     // Set isLoading to false after the delay
-  //     setIsLoading(false);
-  //   }, 2000); // 2000 milliseconds (2 seconds) delay, adjust as needed
-
-  //   // Cleanup the timeout to avoid memory leaks
-  //   return () => clearTimeout(loadingTimeout);
-  // }, []); // Empty dependency array ensures useEffect runs only once
-
   useEffect(() => {
     // Check if window is defined to avoid server-side rendering issues
     if (typeof window !== "undefined") {
       // Import Locomotive Scroll only if window is defined
-      import("locomotive-scroll").then((LocomotiveScroll) => {
+      import("locomotive-scroll").then((module) => {
+        const LocomotiveScroll = module.default;
+
         const scroll = new LocomotiveScroll({
           // Locomotive Scroll options can be configured here
           // For example: smooth scroll, scrollbar, etc.
         });
+
+        // Set isLoading to false after Locomotive Scroll is initialized
+        // setIsLoading(false);
 
         // Cleanup the scroll instance on component unmount
         return () => {
@@ -42,6 +36,10 @@ const Page = () => {
       });
     }
   }, []);
+
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
 
   return (
     <div
